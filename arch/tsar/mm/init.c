c4acf39499e7444cc86f7a889ecdf59590e559e6
Joel Porquet <joel.porquet@lip6.fr>
2014-02-17 17:17:00 +0100

    tsar: concentrate modification of totalram_pages into the mm core
    backport commit c988534737a358fdff42fcce78f0ff1a12dbfc5

--- arch/tsar/mm/init.c
+++ arch/tsar/mm/init.c
@@ -262,7 +262,7 @@ void __init mem_init(void)
 	 * section */
 
 	/* this will put all low memory onto the freelists */
-	totalram_pages += free_all_bootmem();
+	free_all_bootmem();
 
 	reservedpages = 0;
 	for (pfn = 0; pfn < max_low_pfn; pfn++) {
