ebb6af113df8bb7a4ebe2c0df6df1d9c291d8f77
Joel Porquet <joel.porquet@lip6.fr>
2014-02-17 14:18:49 +0100

    irqchip: introduce IRQCHIP_DECLARE macro in linux/irqchip.h
    At the moment the IRQCHIP_DECLARE macro is only declared locally in
    drivers/irqchip/irqchip.h. It prevents from using it directly in arch/*
    directories whenever irqchip drivers only exist there.
    
    This patch makes it global and thus usable for arch-specific
    declarations of irqchip drivers.

--- include/linux/irqchip.h
+++ include/linux/irqchip.h
@@ -12,9 +12,28 @@
 #define _LINUX_IRQCHIP_H
 
 #ifdef CONFIG_IRQCHIP
-void irqchip_init(void);
+extern void irqchip_init(void);
+
+/*
+ * This macro must be used by the different irqchip drivers to declare
+ * the association between their DT compatible string and their
+ * initialization function.
+ *
+ * @name: name that must be unique accross all IRQCHIP_DECLARE of the
+ * same file.
+ * @compstr: compatible string of the irqchip driver
+ * @fn: initialization function
+ */
+#define IRQCHIP_DECLARE(name,compstr,fn)				\
+	static const struct of_device_id irqchip_of_match_##name	\
+	__used __section(__irqchip_of_table)				\
+	= { .compatible = compstr, .data = fn }
 #else
 static inline void irqchip_init(void) {}
+#define IRQCHIP_DECLARE(name,compstr,fn)				\
+	static const struct of_device_id irqchip_of_match_##name	\
+	__attribute__((unused))						\
+	= { .compatible = compstr, .data = fn }
 #endif
 
 #endif
