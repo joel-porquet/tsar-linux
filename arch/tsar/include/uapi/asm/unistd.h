d1a599af8ba5b2d8b727ddf9a845df8ae3d90553
Joel Porquet <joel.porquet@lip6.fr>
2014-02-17 15:12:45 +0100

    tsar: add a couple comments in uapi/unistd.h
    * clone seems to be the only necessary syscall for creating user threads
    * llseek renaming to make it work with libc

--- arch/tsar/include/uapi/asm/unistd.h
+++ arch/tsar/include/uapi/asm/unistd.h
@@ -1,9 +1,12 @@
 #define __ARCH_HAVE_MMU
 
+/* clone seems to be the only necessary syscall for creating user threads: fork
+ * and vfork can be emulated with it - see libc for more details */
 #define __ARCH_WANT_SYS_CLONE
 
 #define sys_mmap2 sys_mmap_pgoff
 
+/* libc expects llseek with another name */
 #define __NR__llseek __NR_llseek
 
 #include <asm-generic/unistd.h>
