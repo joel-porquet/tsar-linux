423a49d007c2d4509e9ecb5b2a2f4cd73e1bb39a
Joel Porquet <joel.porquet@lip6.fr>
2014-02-18 10:54:01 +0100

    tsar: remove HAVE_ARCH_DEVTREE_FIXUPS
    backport commit 32df8dca503f82c816f8be85a8d0a394a8b88c2c

--- arch/tsar/include/asm/prom.h
+++ arch/tsar/include/asm/prom.h
@@ -1,8 +1,6 @@
 #ifndef _ASM_TSAR_PROM_H
 #define _ASM_TSAR_PROM_H
 
-#define HAVE_ARCH_DEVTREE_FIXUPS
-
 extern struct boot_param_header __dtb_start; /* defined by Linux */
 
 #ifdef CONFIG_EARLY_PRINTK
