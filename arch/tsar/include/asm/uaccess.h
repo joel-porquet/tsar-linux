490bdc4d143df8238c2e36d049421986b203a4db
Joel Porquet <joel.porquet@lip6.fr>
2014-02-21 17:55:44 +0100

    tsar: redefine macro __user_ok
    use more generic get_fs() instead of TASK_SIZE

--- arch/tsar/include/asm/uaccess.h
+++ arch/tsar/include/asm/uaccess.h
@@ -2,8 +2,7 @@
 #define _ASM_TSAR_UACCESS_H
 
 #define __kernel_ok		(segment_eq(get_fs(), KERNEL_DS))
-#define __user_ok(addr, size)	(((size) <= TASK_SIZE)			\
-				&& ((addr) <= TASK_SIZE - (size)))
+#define __user_ok(addr, size)	(((size) <= get_fs().seg) && ((addr) <= (get_fs().seg - (size))))
 #define __access_ok(addr, size)	(__kernel_ok || __user_ok((addr), (size)))
 
 #include <asm-generic/uaccess.h>
