63ca93baedd7f556dfdc1263fa39fa25a334f431
Joel Porquet <joel.porquet@lip6.fr>
2014-02-28 20:32:29 +0100

    tsar: add arch-specific atomic.h
    define the four necessary functions for atomic operations for properly
    using the generic atomic.h in case of SMP configuration.

---
+++ arch/tsar/include/asm/atomic.h
@@ -0,0 +1,71 @@
+#ifndef _ASM_TSAR_ATOMIC_H
+#define _ASM_TSAR_ATOMIC_H
+
+#ifdef CONFIG_SMP
+
+/*
+ * Add or substract integer to atomic variable
+ */
+
+#define ATOMIC_RETURN(fn, op)                                       \
+static inline int __tsar_atomic_return_##fn(int i, atomic_t *v)     \
+{                                                                   \
+	int res, tmp;                                               \
+	smp_mb__before_llsc();                                      \
+	__asm__ __volatile__(                                       \
+			".set push				\n" \
+			".set noreorder				\n" \
+			"1:	ll	%[tmp], %[mem]		\n" \
+			"	" #op "	%[res], %[tmp], %[imm]	\n" \
+			"	sc	%[res], %[mem]		\n" \
+			".set pop				\n" \
+			"	beqz	%[res], 1b		\n" \
+			"	" #op "	%[res], %[tmp], %[imm]	\n" \
+			: [tmp] "=&r" (tmp), [res] "=&r" (res),     \
+			[mem] "+m" (v->counter)                     \
+			: [imm] "Ir" (i));                          \
+	smp_mb__after_llsc();                                       \
+	return res;                                                 \
+}
+
+ATOMIC_RETURN(add, addu);
+ATOMIC_RETURN(sub, subu);
+
+#define atomic_add_return(i, v) __tsar_atomic_return_add(i, v)
+#define atomic_sub_return(i, v) __tsar_atomic_return_sub(i, v)
+
+
+/*
+ * Clear or set bits in atomic variable
+ */
+
+#define ATOMIC_MASK(fn, op)                                                 \
+static inline void __tsar_atomic_mask_##fn(unsigned long mask, atomic_t *v) \
+{                                                                           \
+	int tmp;                                                            \
+	smp_mb__before_llsc();                                              \
+	__asm__ __volatile__(                                               \
+			".set push				\n"         \
+			".set noreorder				\n"         \
+			"1:	ll	%[tmp], %[mem]		\n"         \
+			"	" #op "	%[tmp], %[mask]		\n"         \
+			"	sc	%[tmp], %[mem]		\n"         \
+			".set pop				\n"         \
+			"	beqz	%[tmp], 1b		\n"         \
+			: [tmp] "=&r" (tmp),                                \
+			[mem] "+m" (v->counter)                             \
+			: [mask] "Ir" (mask));                              \
+	smp_mb__after_llsc();                                               \
+}
+
+ATOMIC_MASK(clear, and);
+ATOMIC_MASK(set, or);
+
+#define atomic_clear_mask(m, v) __tsar_atomic_mask_clear(~(m), v)
+#define atomic_set_mask(m, v) __tsar_atomic_mask_set(m, v)
+
+#endif /* CONFIG_SMP */
+
+#include <asm-generic/atomic.h>
+
+#endif /* _ASM_TSAR_ATOMIC_H */
