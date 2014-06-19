ca8c69af118ae835f0965aea3d474d3b4ae0f6dc
Joel Porquet <joel.porquet@lip6.fr>
2014-06-19 16:19:19 +0200

    tsar: early_printk: adaptation to 40-bit addressing
    Fix the early_printk code so that it properly deals with a 40-bit
    address for where the early tty is physically mapped.

--- arch/tsar/kernel/early_printk.c
+++ arch/tsar/kernel/early_printk.c
@@ -14,12 +14,15 @@
 
 #include <asm/io.h>
 
+/* define a few things about SoCLib vci_multi_tty */
+#define VCI_TTY_WRITE	0x0	/* offset for writing operation */
+#define VCI_TTY_SIZE	0x10	/* size of the tty segment */
+
 static void __iomem *vci_tty_virt_base;
 
 static void early_tty_putchar(const char c)
 {
 	/* SoCLib vci_multi_tty register map (short) */
-	#define VCI_TTY_WRITE 0x0
 
 	__raw_writel(c, vci_tty_virt_base + VCI_TTY_WRITE);
 
@@ -54,6 +57,7 @@ static char *stdout_path __initdata;
 static int __init early_init_dt_scan_chosen_tty(unsigned long node, const char *uname,
 		int depth, void *data)
 {
+	phys_addr_t *tty_base_addr = data;
 	unsigned long l;
 	char *p;
 
@@ -78,9 +82,10 @@ static int __init early_init_dt_scan_chosen_tty(unsigned long node, const char *
 		pr_debug("linux,stdout-path compatible string: %s\n", p);
 
 		if (strcmp(p, "soclib,vci_multi_tty") == 0) {
-			phys_addr_t *base_addr;
-			base_addr = of_get_flat_dt_prop(node, "reg", &l);
-			return be32_to_cpup(base_addr);
+			void *a;
+			a = of_get_flat_dt_prop(node, "reg", &l);
+			*tty_base_addr = be64_to_cpup(a);
+			return 1;
 		}
 	}
 
@@ -89,9 +94,9 @@ static int __init early_init_dt_scan_chosen_tty(unsigned long node, const char *
 }
 
 /* this function works while the DTB is still unflattened */
-static phys_addr_t __init of_early_console(void)
+static int __init of_early_console(phys_addr_t *console_addr)
 {
-	return of_scan_flat_dt(early_init_dt_scan_chosen_tty, NULL);
+	return of_scan_flat_dt(early_init_dt_scan_chosen_tty, console_addr);
 }
 
 static int __init setup_early_printk(char *buf)
@@ -103,17 +108,14 @@ static int __init setup_early_printk(char *buf)
 		return 0;
 
 	/* setup the physical address of the early console device */
-	vci_tty_base_addr = of_early_console();
-
-	if (!vci_tty_base_addr)
+	if (!of_early_console(&vci_tty_base_addr))
 		/* no tty is available... */
 		return 0;
 
 	/* map the tty */
-	vci_tty_virt_base = ioremap_nocache(vci_tty_base_addr, 0xc);
+	vci_tty_virt_base = ioremap_nocache(vci_tty_base_addr, VCI_TTY_SIZE);
 
-	if (!vci_tty_virt_base)
-		panic("Failed to remap early_printk memory\n");
+	BUG_ON(!vci_tty_virt_base);
 
 	early_console = &early_tty_console;
 	register_console(early_console);
